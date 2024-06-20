566712b4b7fc41d2db73cbb89640de4c6fe06b74
dvarkey <d.varkey@outlook.com>
2024-06-20 11:51:01 -0230

    split r from moratorium

--- analysis/WF/runWF.R
+++ analysis/WF/runWF.R
@@ -7,5 +7,8 @@ parameters <- WFdata$parameters
 mod1=writeBSPmodel(rprior=0.2000379)
 mod2=writeBSPmodel(rprior=0.1)
 
-vv=runModel(modeltxt=textConnection(mod1), data=data,inits=inits, parameters=parameters, n.chains=3, n.adapt=100, n.burnin=1000, n.iter=2000,n.thin=10)
-vp=runProjections(nY=3,output=vv,Yield)
+#mod3=writeBSPmodel(rprior=0.2000379,split=TRUE);parameters=c(WFdata$parameters,"r1")
+
+
+vv=runModel(modeltxt=textConnection(mod3), data=data,inits=inits, parameters=parameters, n.chains=3, n.adapt=100, n.burnin=1000, n.iter=2000,n.thin=10)
+vp=runProjections(nY=3,output=vv,Yield=c(0.4,0.5,0.6))
